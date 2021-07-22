11f667492bf746093c6ee0cb4f85c57285a28479
suyb <suyb@suybdeMacBook-Pro.local>
2021-07-22 21:53:05 +0800

    netty http server example

---
+++ src/main/java/com/demo/netty/http/TestServer.java
@@ -0,0 +1,32 @@
+package com.demo.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+            ChannelFuture channelFuture = serverBootstrap.bind(6668).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+
+
+
+    }
+
+}
