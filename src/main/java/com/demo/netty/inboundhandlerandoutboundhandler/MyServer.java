ffe557e3589aaaeabfddaeca49625c3a87521b5d
suyb <suyb@suybdeMacBook-Pro.local>
2021-08-02 20:34:26 +0800

    inboundhandlerandoutbandhandler 事例

---
+++ src/main/java/com/demo/netty/inboundhandlerandoutboundhandler/MyServer.java
@@ -0,0 +1,29 @@
+package com.demo.netty.inboundhandlerandoutboundhandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(7000).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+}
