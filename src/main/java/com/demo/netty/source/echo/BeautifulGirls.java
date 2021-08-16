617e2d8d50dde04086d074d7e75180b068fbadc1
suyb <suyb@suybdeMacBook-Pro.local>
2021-08-17 00:16:54 +0800

    rpc

---
+++ src/main/java/com/demo/netty/source/echo/BeautifulGirls.java
@@ -0,0 +1,50 @@
+package com.demo.netty.source.echo;
+
+public class BeautifulGirls {
+
+    private Integer age;
+    private Integer facialAttractiveness;
+    private String name;
+    private Float weight;
+    private Float height;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getFacialAttractiveness() {
+        return facialAttractiveness;
+    }
+
+    public void setFacialAttractiveness(Integer facialAttractiveness) {
+        this.facialAttractiveness = facialAttractiveness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+}
